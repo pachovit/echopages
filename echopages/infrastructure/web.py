648a91e8d9765fc2fe3486cee04902a5f172e71f
Pachovit <github@mail.pachovit.com>
2024-07-14 10:53:24 +0200

    Add content endpoint

---
+++ echopages/infrastructure/web.py
@@ -0,0 +1,77 @@
+from fastapi import Depends, FastAPI, status
+from pydantic import BaseModel
+
+from echopages.application import services
+from echopages.domain.repositories import ContentRepository
+from echopages.infrastructure.fakes import FakeContentRepository
+
+app = FastAPI()
+
+content_repo = None
+
+
+def get_content_repo():
+    global content_repo
+    if content_repo is None:
+        content_repo = FakeContentRepository([])
+    return content_repo
+
+
+class Content(BaseModel):
+    text: str
+
+
+class AddContentResponse(BaseModel):
+    content_unit_id: str
+
+
+class GetContentUnitResponse(BaseModel):
+    text: str
+
+
+@app.post("/add_content", status_code=status.HTTP_201_CREATED)
+async def add_content(
+    content: Content,
+    content_repo: ContentRepository = Depends(get_content_repo),
+):
+    # Simulate adding content
+
+    content_unit_id = services.add_content(content_repo, content.text)
+    return AddContentResponse(content_unit_id=content_unit_id)
+
+
+@app.get("/content_units/{content_unit_id}")
+async def get_content_unit(
+    content_unit_id: str,
+    content_repo: ContentRepository = Depends(get_content_repo),
+):
+    content_unit = content_repo.get_by_id(content_unit_id)
+
+    return (
+        GetContentUnitResponse(text=content_unit.text)
+        if content_unit
+        else {"error": f"Content unit with id {content_unit_id} not found"}
+    )
+
+
+# class Schedule(BaseModel):
+#     time_of_day: str = Field(
+#         ...,
+#         regex=r"^([01]\d|2[0-3]):([0-5]\d)$",  # regex for HH:MM format
+#         example="12:30",
+#     )
+
+# @app.post("/configure_schedule")
+# async def configure_schedule(schedule: Schedule):
+#     digest_repo = repositories.DigestRepository([])
+#     content_repo = repositories.ContentRepository([])
+#     content_sampler = samplers.SimpleContentSampler()
+#     number_of_units = config.NUMBER_OF_UNITS_PER_DIGEST
+#     scheduler = schedulers.SimpleScheduler(
+#         services.generate_digest(
+#             digest_repo, content_repo, content_sampler, number_of_units
+#         )
+#     )
+#     services.configure_schedule(scheduler, schedule.time_of_day)
+#     scheduler.update_schedule(schedule.time_of_day)
+#     return {"message": "Schedule updated"}
---
+++ tests/integration/test_web.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+
+from echopages.infrastructure import web
+from echopages.infrastructure.fakes import FakeContentRepository
+
+content_repo = None
+
+
+def get_content_repo():
+    global content_repo
+    if content_repo is None:
+        content_repo = FakeContentRepository([])
+    return content_repo
+
+
+client = TestClient(web.app)
+web.app.dependency_overrides[web.get_content_repo] = lambda: get_content_repo()
+
+
+def test_add_content_unit():
+    content_unit = "sample content unit 1"
+    url = "http://127.0.0.1:8000"
+
+    r = client.post(f"{url}/add_content", json={"text": content_unit})
+    assert r.status_code == 201
+
+    content_unit_id = r.json()["content_unit_id"]
+
+    r = client.get(f"{url}/content_units/{content_unit_id}")
+    assert r.status_code == 200
+    assert r.json()["text"] == content_unit
+
+
+# def test_configure_schedule():
+#     url = "http://127.0.0.1:8000"
+
+#     r = requests.post(f"{url}/configure_schedule", json={"time_of_day": "07:00"})
+
+#     assert r.ok
+
+
+# def test_generate_digest():
+#     url = "http://127.0.0.1:8000"
+#     number_of_units = 2
+#     content = "sample content unit 1"
+#     r = requests.post(f"{url}/add_content", json={"text": content})
+
+#     r = requests.post(
+#         f"{url}/generate_digest", json={"number_of_units": number_of_units}
+#     )
+#     assert r.ok
+
+#     digest = r.json()["digest"]
+#     assert len(digest) == number_of_units
+#     assert all(digest == [content for _ in range(number_of_units)])
