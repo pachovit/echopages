cdabeec43d5f8042bf2d7431f4c79e3fd69ea7aa
G L <gitlab@mail.glem.cc>
2024-07-26 23:53:46 +0200

    Added Dockerfiles

--- echopages/config.py
+++ echopages/config.py
@@ -1,9 +1,10 @@
 import os
 
-DB_URI = os.environ.get("DB_URI", "echopages.db")
+DB_URI = os.environ.get("DB_URI", "data/echopages.db")
 DELIVERY_SYSTEM = os.environ.get("DELIVERY_SYSTEM", "PostmarkDigestDeliverySystem")
 DISK_DELIVERY_SYSTEM_DIRECTORY = os.environ.get(
-    "DISK_DELIVERY_SYSTEM_DIRECTORY", "./digests"
+    "DISK_DELIVERY_SYSTEM_DIRECTORY", "data/digests"
 )
 RECIPIENT_EMAIL = os.environ.get("RECIPIENT_EMAIL", "hello@echopages.com")
-NUMBER_OF_UNITS_PER_DIGEST = 1
+NUMBER_OF_UNITS_PER_DIGEST = int(os.environ.get("NUMBER_OF_UNITS_PER_DIGEST", 1))
+DAILY_TIME_OF_DIGEST = os.environ.get("DAILY_TIME_OF_DIGEST", "07:00")
