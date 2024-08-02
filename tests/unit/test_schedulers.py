f0bc7d14f91eb4641a26da44a12965f49660fb12
pachovit <github@mail.pachovit.com>
2024-08-02 14:33:36 +0000

    Persistent config and reconfigurable schedule

--- tests/unit/test_schedulers.py
+++ tests/unit/test_schedulers.py
@@ -7,17 +7,20 @@ from zoneinfo import ZoneInfo
 from echopages.infrastructure.delivery import schedulers
 
 
+class DummyClass:
+    value = 0
+
+
+def dummy_function() -> None:
+    DummyClass.value = 1
+
+
 def test_scheduler_works_on_time() -> None:
-    class DummyClass:
-        value = 0
-
-    def dummy_function() -> None:
-        DummyClass.value = 1
-
+    DummyClass.value = 0
     zone = ZoneInfo("Europe/Berlin")
     original_time = datetime(2020, 1, 1, 6, 59, 59, tzinfo=zone)
     with travel(original_time, tick=False) as traveller:
-        # Given: A scheduler and some digests
+        # Given: A scheduler
         scheduler = schedulers.SimpleScheduler(
             dummy_function, "07:00", sleep_interval=0.05
         )
@@ -25,6 +28,29 @@ def test_scheduler_works_on_time() -> None:
 
         # When: Schedule time arrives
         scheduler.start()
+        traveller.shift(timedelta(seconds=2))
+        time.sleep(0.1)
+        scheduler.stop()
+
+        # Then: Digest is sent
+        assert DummyClass.value == 1
+
+
+def test_scheduler_is_reconfigured() -> None:
+    DummyClass.value = 0
+
+    zone = ZoneInfo("Europe/Berlin")
+    original_time = datetime(2020, 1, 1, 6, 59, 59, tzinfo=zone)
+    with travel(original_time, tick=False) as traveller:
+        # Given: A running scheduler
+        scheduler = schedulers.SimpleScheduler(
+            dummy_function, "06:00", sleep_interval=0.05
+        )
+        scheduler.start()
+        assert DummyClass.value == 0
+
+        # When: Schedule time arrives
+        scheduler.configure_schedule("07:00")
         traveller.shift(timedelta(seconds=2))
         time.sleep(0.1)
         scheduler.stop()
