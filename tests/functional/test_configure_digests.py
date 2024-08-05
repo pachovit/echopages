21b727a39b12ad38fda6a1ec1989eac365248c78
pachovit <github@mail.pachovit.com>
2024-08-05 20:13:41 +0200

    Fixed mypy issues

--- tests/functional/test_configure_digests.py
+++ tests/functional/test_configure_digests.py
@@ -1,7 +1,8 @@
+from typing import Any, Dict
+
 from fastapi.testclient import TestClient
-from httpx import Response
 from pytest_bdd import given, scenario, then, when
-from typing import Dict, Any
+
 from echopages.api import endpoints
 
 client = TestClient(endpoints.app)
@@ -14,26 +15,29 @@ def test_reconfigure_digest() -> None:
 
 
 @given("a previous configuration", target_fixture="config")
-def get_config() -> None:
+def get_config() -> Dict[str, Any]:
     r = client.get(f"{host_url}/get_config")
     assert r.status_code == 200, f"Failed with status {r.status_code}: {r.json()}"
-    return r.json()
+    return r.json()  # type: ignore
 
 
 @when("I send new configuration parameters", target_fixture="new_config_params")
-def update_config(config) -> Response:
+def update_config(config: Dict[str, Any]) -> Dict[str, Any]:
     new_number_of_units_per_digest = config["number_of_units_per_digest"] + 1
     new_daily_time_of_digests = "12:34"
     assert config["daily_time_of_digest"] != new_daily_time_of_digests
-    config = {"number_of_units_per_digest": new_number_of_units_per_digest,"daily_time_of_digest": new_daily_time_of_digests}
-    
+    config = {
+        "number_of_units_per_digest": new_number_of_units_per_digest,
+        "daily_time_of_digest": new_daily_time_of_digests,
+    }
+
     r = client.post(f"{host_url}/set_config", json=config)
     assert r.status_code == 201, f"Failed with status {r.status_code}: {r.json()}"
     return config
 
 
 @then("the configuration is updated")
-def config_is_updated(new_config_params: Dict[str, Any]) -> None:    
+def config_is_updated(new_config_params: Dict[str, Any]) -> None:
     new_number_of_units_per_digest = new_config_params["number_of_units_per_digest"]
     new_daily_time_of_digests = new_config_params["daily_time_of_digest"]
     r = client.get(f"{host_url}/get_config")
